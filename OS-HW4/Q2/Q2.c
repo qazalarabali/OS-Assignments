24579de1da104f0db57ca92e45120e67ab3fe134
qazalarabali <qazalarabali79@gmail.com>
2023-12-28 17:24:25 +0330

    Document added

--- OS-HW4/Q2/Q2.c
+++ OS-HW4/Q2/Q2.c
@@ -7,34 +7,38 @@
 sem_t sem1, sem2;
 
 void* p1(void* arg) {
-    sem_wait(&sem2); // Wait for sem2 to be available
+
+    sem_wait(&sem2); 
     printf("F\n");
+
     sem_post(&sem1);
     printf("E\n");
     printf("G\n");
-    //sem_post(&sem1); // Release sem1 to allow p2 to proceed
+
     return NULL;
 }
 
 void* p2(void* arg) {
-    sem_post(&sem2); // Wait for sem1 to be available
+
+    
     printf("A\n");
-    //sem_post(&sem2); // Release sem2 to allow p1 to proceed
-    sem_wait(&sem1); // Wait for sem1 to be available
+    sem_post(&sem2); 
+
+    sem_wait(&sem1); 
     printf("C\n");
     printf("B\n");
-    //sem_post(&sem2); // Release sem2
+
+    printf("C\n");
+    printf("B\n");
     return NULL;
 }
 
 int main() {
+    
     // semaphore initializations
     sem_init(&sem2, 0, 0);
     sem_init(&sem1, 0, 0);
-    // if (sem_init(&sem1, 0, 0) != 0 || sem_init(&sem2, 0, 1) != 0) {
-    //     perror("Semaphore initialization failed");
-    //     return 1;
-    // }
+
 
     // thread creations
     pthread_t thread1, thread2;
--- OS-HW4/Q2/Q2_1.c
+++ OS-HW4/Q2/Q2_1.c
@@ -6,36 +6,34 @@
 sem_t sem_p1, sem_p2, sem_p3;
 
 void *p1(void *arg) {
+
     // F
-    sem_wait(&sem_p2); // Wait for signal from p2 to print F
+    sem_wait(&sem_p2); 
     printf("F\n");
 
     // E
-    sem_post(&sem_p3); // Signal that E can be printed
-    sem_wait(&sem_p1); // Wait for signal from p2 to print E
+    sem_post(&sem_p3); 
+    sem_wait(&sem_p1); 
     printf("E\n");
 
     // G
     printf("G\n");
-
     pthread_exit(NULL);
 }
 
 void *p2(void *arg) {
+
     // A
     printf("A\n");
-    sem_post(&sem_p2); // Signal that A is done
-
-    sem_wait(&sem_p3); // Wait for signal from p1 that E can be printed
+    sem_post(&sem_p2); 
+    sem_wait(&sem_p3); 
 
     // C
     printf("C\n");
 
     // B
     printf("B\n");
-
-    sem_post(&sem_p1); // Signal that E is done
-
+    sem_post(&sem_p1);
     pthread_exit(NULL);
 }
 
